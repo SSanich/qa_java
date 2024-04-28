446118137e05741d6d5b999bbe371e135339c45e
Aleksandr Grigoriev <sanich.grig@gmail.com>
2024-04-28 23:47:14 +0700

    fix_cat_test

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -3,25 +3,34 @@ import com.example.Feline;
 import com.example.Names;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import static org.junit.Assert.assertEquals;
 
 
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
+    @Mock
     Feline feline;
-    Cat cat;
+    Cat cat ;
 
     @Before
     public void setup() {
-        feline = new Feline();
         cat = new Cat(feline);
     }
+
     @Test
     public void testGetSound() {
         assertEquals("Мяу", cat.getSound());
     }
     @Test
     public void testGetFood() throws Exception {
-        assertEquals(Names.FOR_PREDATOR_LIST, cat.getFood());
+        Mockito.when(feline.eatMeat()).thenReturn(Names.FOR_PREDATOR_LIST);
+        cat.getFood();
+        Mockito.verify(feline, Mockito.times(1)).eatMeat();
     }
 
 }
